620d1e5aca950e117638188037d7497ef901cfab
wooknick <wooknick@naver.com>
2022-03-29 03:30:33 +0900

    "feat" fetch data logic

--- src/injection/index.js
+++ src/injection/index.js
@@ -1,1 +1,65 @@
-console.log("injection");
+import qs from "query-string";
+(async function () {
+  await relatedVideoLoaded();
+  getRelatedVideos();
+
+  let scrollT = undefined;
+  window.addEventListener("scroll", () => {
+    if (scrollT) {
+      clearTimeout(scrollT);
+    }
+    scrollT = setTimeout(getRelatedVideos, 700);
+  });
+
+  function getCurrent() {
+    let ret = undefined;
+    const url = window.location.href;
+    const parsedUrl = qs.parseUrl(url);
+    if (Object.keys(parsedUrl.query).includes("v")) {
+      ret = parsedUrl.query.v;
+    }
+    return ret;
+  }
+
+  function getRelatedVideos() {
+    const relatedVideos = new Set();
+    const relativeList = document.querySelectorAll(
+      "div#contents.style-scope.ytd-item-section-renderer > ytd-compact-video-renderer"
+    );
+    for (const item of relativeList) {
+      const url = item.querySelector("a").href;
+      const parsedUrl = qs.parseUrl(url);
+      if (Object.keys(parsedUrl.query).includes("v")) {
+        const id = parsedUrl.query.v;
+        relatedVideos.add(id);
+      }
+    }
+
+    console.log(JSON.stringify([...relatedVideos]));
+    chrome.runtime.sendMessage({
+      payload: "youtubeLink",
+      data: {
+        current: getCurrent(),
+        relatedVideos: [...relatedVideos],
+      },
+    });
+    return relatedVideos;
+  }
+
+  function relatedVideoLoaded() {
+    return new Promise((resolve) => {
+      observe();
+      function observe() {
+        const relatedList = document.querySelectorAll(
+          "div#contents.style-scope.ytd-item-section-renderer > ytd-compact-video-renderer"
+        );
+        if (relatedList.length > 5) {
+          console.log(">>> Relative Video Loaded");
+          resolve();
+        } else {
+          setTimeout(observe, 1000 / 30);
+        }
+      }
+    });
+  }
+})();
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,1 +1,20 @@
-console.log("popup");
+const app = document.querySelector("div#app");
+
+chrome.runtime.sendMessage({ payload: "getData" }, (response) => {
+  const { data } = response;
+  const keys = Object.keys(data);
+  keys.forEach((key) => {
+    app.appendChild(createElement(key, 0));
+    data[key].forEach((d) => {
+      app.appendChild(createElement(d, 1));
+    });
+    app.appendChild(createElement("   ", 0));
+  });
+});
+
+function createElement(id, depth = 0) {
+  const div = document.createElement("div");
+  div.classList = "line";
+  div.innerText = "-".repeat(depth * 4) + id;
+  return div;
+}
